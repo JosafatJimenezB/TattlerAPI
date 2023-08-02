8d719bc51f8677ad8a71d99032e14d09abeb6a07
josafat <josa221001@gmail.com>
2023-08-02 17:55:49 -0600

    add: agregando cors y el router

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express, { Application, Request, Response } from 'express';
-import connectToDatabase from './config/db'; // Importa la función desde db.ts
+import connectToDatabase from './config/db';
 import 'dotenv/config';
+import apiRouter from './routes/restaurantRoutes' // Importa la función desde db.ts
+import cors from 'cors'
 
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT || '3000');
 
 app.use(express.json());
 
-// Conexión a la base de datos MongoDB
 connectToDatabase();
 
-// Resto del código sigue igual...
+app.use(cors());
+
+app.use('/api', apiRouter)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
